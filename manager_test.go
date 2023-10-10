d1a28c6109acec59e82cf8088ea53d484c8fb2d3
Dalton Pinto <dalton.teixeira@stone.com.br>
2023-10-10 10:18:17 +0200

    Cover cases where operation is not persisted

--- manager_test.go
+++ manager_test.go
@@ -257,10 +257,274 @@ func TestNeverTimeoutRunningOperation(t *testing.T) {
 
 	exptectedErr := newStillRunningError("target", "key")
 	if err == nil || err.Error() != exptectedErr.Error() {
-		t.Fatalf("Exptected to have \"%v\" error, but got \"%v\"", exptectedErr, err)
+		t.Fatalf("Expected to have \"%v\" error, but got \"%v\"", exptectedErr, err)
 	}
 
 	if result != nil {
-		t.Fatalf("Exptected to have no result, but got \"%s\"", result.result)
+		t.Fatalf("Expected to have no result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNeverExpiredAndFinishedOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Finished,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Time{},
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		time.Duration(0),
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "tracked result" {
+		t.Fatalf("Expected to have \"tracked result\" as result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNeverExpiredAndReadyOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Ready,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Time{},
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		time.Duration(0),
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "result" {
+		t.Fatalf("Expected to have \"result\" as result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNeverExpiredAndRunningOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Running,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Time{},
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		time.Duration(0),
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "result" {
+		t.Fatalf("Expected to have \"result\" as result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNeverExpiredAndFailedOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Failed,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Time{},
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		time.Duration(0),
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "result" {
+		t.Fatalf("Expected to have \"result\" as result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNotExpiredAndFinishedOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Finished,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(10*time.Second),
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		10*time.Second,
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "tracked result" {
+		t.Fatalf("Expected to have \"tracked result\" as result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNotExpiredAndReadyOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Ready,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(10*time.Second),
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		10*time.Second,
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "result" {
+		t.Fatalf("Expected to have \"result\" as result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNotExpiredAndRunningOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Running,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(10*time.Second),
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		10*time.Second,
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "result" {
+		t.Fatalf("Expected to have \"result\" as result, but got \"%s\"", result.result)
+	}
+}
+
+func TestNotExpiredAndFailedOperation(t *testing.T) {
+	trackedOperation := NewTrackedOperation(
+		Failed,
+		"key",
+		"target",
+		"payload",
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(-7*time.Second),
+		time.Now().Add(-10*time.Second),
+		time.Now().Add(10*time.Second),
+		newMockedResult("tracked result"),
+	)
+	manager := New(newTrackedOperationRepository(trackedOperation))
+	operation := newMockedOperation(
+		"key",
+		"target",
+		"payload",
+		time.Now(),
+		5*time.Second,
+		10*time.Second,
+		newMockedResult("result"),
+	)
+	result, err := manager.Call(operation)
+
+	if err != nil {
+		t.Fatalf("Expected to have no error, but got \"%v\"", err)
+	}
+
+	if result == nil || result.result != "result" {
+		t.Fatalf("Expected to have \"result\" as result, but got \"%s\"", result.result)
 	}
 }
